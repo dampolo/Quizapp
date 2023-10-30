069964f9a0e57f29edb8311dfb76add5e3c73ce9
Damian Poloczek <damianpoloczek@gmail.com>
2023-10-30 21:14:02 +0100

    first part is ready

--- script.js
+++ script.js
@@ -23,58 +23,60 @@ let questions = [
         "answer-4": "html",
         "correct-answer": 1,
     },
-    // {
-    //     "question": "Was befindet sich im img-Tag?",
-    //     "answer-1": "Video",
-    //     "answer-2": "Sound",
-    //     "answer-3": "Text",
-    //     "answer-4": "Bild",
-    //     "correct-answer": 4,
-    // },
-    // {
-    //     "question": "Was macht oder ist das CSS?",
-    //     "answer-1": "Server",
-    //     "answer-2": "Texte",
-    //     "answer-3": "Styling",
-    //     "answer-4": "Zahlungen",
-    //     "correct-answer": 3,
-    // },
-    // {
-    //     "question": "Wofür benötigt man ein script-Tag?",
-    //     "answer-1": "Einbinden von JavaScript",
-    //     "answer-2": "Einbinden von CSS",
-    //     "answer-3": "Einbinden von HTML",
-    //     "answer-4": "Einbinden von Bootstrap",
-    //     "correct-answer": 1,
-    // },
-    // {
-    //     "question": "Wofür steht CSS?",
-    //     "answer-1": "Creating Styling Sheets",
-    //     "answer-2": "Computing Sty Sheets",
-    //     "answer-3": "Cascading Style Sheets",
-    //     "answer-4": "Color Sheme Sheets",
-    //     "correct-answer": 3,
-    // },
-    // {
-    //     "question": "Wie definiert man eine JavaScript-Funktion?",
-    //     "answer-1": "myFunction()",
-    //     "answer-2": "function myFunction()",
-    //     "answer-3": "function=myFunction()",
-    //     "answer-4": "function:myFunction()",
-    //     "correct-answer": 2,
-    // },
-    // {
-    //     "question": "Wie schreibt man eine IF-Abfrage in JavaScript?",
-    //     "answer-1": "if(index : 0)",
-    //     "answer-2": "if(index 0)",
-    //     "answer-3": "if{index = 0)}",
-    //     "answer-4": "if(index == 0)",
-    //     "correct-answer": 4,
-    // },
+    {
+        "question": "Was befindet sich im img-Tag?",
+        "answer-1": "Video",
+        "answer-2": "Sound",
+        "answer-3": "Text",
+        "answer-4": "Bild",
+        "correct-answer": 4,
+    },
+    {
+        "question": "Was macht oder ist das CSS?",
+        "answer-1": "Server",
+        "answer-2": "Texte",
+        "answer-3": "Styling",
+        "answer-4": "Zahlungen",
+        "correct-answer": 3,
+    },
+    {
+        "question": "Wofür benötigt man ein script-Tag?",
+        "answer-1": "Einbinden von JavaScript",
+        "answer-2": "Einbinden von CSS",
+        "answer-3": "Einbinden von HTML",
+        "answer-4": "Einbinden von Bootstrap",
+        "correct-answer": 1,
+    },
+    {
+        "question": "Wofür steht CSS?",
+        "answer-1": "Creating Styling Sheets",
+        "answer-2": "Computing Sty Sheets",
+        "answer-3": "Cascading Style Sheets",
+        "answer-4": "Color Sheme Sheets",
+        "correct-answer": 3,
+    },
+    {
+        "question": "Wie definiert man eine JavaScript-Funktion?",
+        "answer-1": "myFunction()",
+        "answer-2": "function myFunction()",
+        "answer-3": "function=myFunction()",
+        "answer-4": "function:myFunction()",
+        "correct-answer": 2,
+    },
+    {
+        "question": "Wie schreibt man eine IF-Abfrage in JavaScript?",
+        "answer-1": "if(index : 0)",
+        "answer-2": "if(index 0)",
+        "answer-3": "if{index = 0)}",
+        "answer-4": "if(index == 0)",
+        "correct-answer": 4,
+    },
 ];
 
 let currentQuestions = 0;
-let rightQuestions = 0
+let rightQuestions = 0;
+let AUDIO_SUCCESS = new Audio('./sounds/success.mp3')
+let AUDIO_FAIL = new Audio('./sounds/wrong.mp3')
 
 function init() {
     document.getElementById("all-questions-amounts").innerHTML = questions.length;
@@ -82,23 +84,30 @@ function init() {
 }
 
 function showQuestion() {
-
     if(currentQuestions >= questions.length) {
+        // Show end screen
         document.getElementById("end-screen").style = ""
         document.getElementById("question-body").style.display = "none"
         document.getElementById("amounts-of-questions").innerHTML = questions.length;
         document.getElementById("amount-of-right-questions").innerHTML = rightQuestions
         document.getElementById("header-image").src = "img/trophy.png";
+        
+        
+    } else {// Show question
+        
+        let precent = (((currentQuestions+1) / questions.length) * 100).toFixed(0);
 
-    } else {
-    let question = questions[currentQuestions]
+        document.getElementById("progress-bar").style.width = `${precent}%`
+        document.getElementById("progress-bar").innerHTML= `${precent}%`
 
-    document.getElementById("number-question").innerHTML = currentQuestions+1
-    document.getElementById("question-text").innerHTML = question["question"]
-    document.getElementById("answer-1").innerHTML = question["answer-1"]
-    document.getElementById("answer-2").innerHTML = question["answer-2"]
-    document.getElementById("answer-3").innerHTML = question["answer-3"]
-    document.getElementById("answer-4").innerHTML = question["answer-4"]
+        let question = questions[currentQuestions]
+
+        document.getElementById("number-question").innerHTML = currentQuestions+1
+        document.getElementById("question-text").innerHTML = question["question"]
+        document.getElementById("answer-1").innerHTML = question["answer-1"]
+        document.getElementById("answer-2").innerHTML = question["answer-2"]
+        document.getElementById("answer-3").innerHTML = question["answer-3"]
+        document.getElementById("answer-4").innerHTML = question["answer-4"]
     }
 }
 
@@ -109,12 +118,12 @@ function answer(selection) {
 
     if (selectedQuestionNumber == question["correct-answer"]) {
         document.getElementById(selection).classList.add("bg-success");
-        //rightQuestions++ // +=1
-        rightQuestions+=1
+        rightQuestions++
+        AUDIO_SUCCESS.play();
     } else {
-        console.log("FALSCH")
         document.getElementById(selection).classList.add("bg-danger")
         document.getElementById(idOfRightAnswer).classList.add("bg-success")
+        AUDIO_FAIL.play();
     }
     document.getElementById("button").disabled = false;
 }
@@ -137,5 +146,11 @@ function resetAnswers() {
     document.getElementById("answer-4").classList.remove("bg-success")
 }
 
-function nuberQuestion() {
+function restartGame() {
+    document.getElementById("header-image").src = "./img/quiz.png";
+    document.getElementById("question-body").style = ""; //
+    document.getElementById("end-screen").style = "display: none"; //Endscreen ausblenden
+    currentQuestions = 0;
+    rightQuestions = 0
+    init();
 }
